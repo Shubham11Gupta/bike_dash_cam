c6beead56aee69e88bca0aaf0a621000a1346d5e
SHUBHAM GUPTA <shubham11gupta10@gmail.com>
2026-09-02 09:56:38 +0530

    Reset for manual coding

--- tests/TestCases.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace bike_dashcam::tests {
-
-bool runConfigManagerTests();
-bool runCameraManagerTests();
-bool runDashcamControllerTests();
-
-}  // namespace bike_dashcam::tests
